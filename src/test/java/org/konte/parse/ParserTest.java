6851bed8f454106640432fa43c8acad8a0839b94
Paavo Toivanen <paavo.v.toivanen@gmail.com>
2015-02-24 00:52:09 +0200

    test corrected

--- src/test/java/org/konte/parse/ParserTest.java
+++ src/test/java/org/konte/parse/ParserTest.java
@@ -13,7 +13,7 @@ public class ParserTest {
     public void testParseExpr() throws ParseException, IllegalArgumentException, IllegalAccessException {
         String s =
 "cube {\n" +
-"    featurez{x 1/3 y -1/3 z -1+2+3}\n" +
+"    SQUARE{x 1/3 y -1/3 z -1+2+3}\n" +
 "}"
 ;
         Model m = new Parser().parse( 
